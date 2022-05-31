4bf5ba3fc136f55c74c8fe83d65a47d57b3fe10f
Aleyna Mandacı <aleynamandaci@gmail.com>
2022-05-31 16:43:52 +0300

    Add func
    Getting data from second view with closure and editting labels with this datas. Also added switch case statement for bmi health range

--- homework-2-aleynamandaci/homework-2-aleynamandaci/ViewController.swift
+++ homework-2-aleynamandaci/homework-2-aleynamandaci/ViewController.swift
@@ -10,14 +10,45 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var bmiLabel: UILabel!
-    @IBOutlet weak var explanationText: UITextView!
-    
+    @IBOutlet weak var rangeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let viewController = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        viewController.modalPresentationStyle = .formSheet
+        
+        //editing label with getting data from second view
+        viewController.completionHandlerName = {text in
+            self.bmiLabel.text = "Hey " + text! + "!"
+        }
+        
+        //editing label with getting data from second view
+        viewController.completionHandlerBmi = {text in
+            self.bmiLabel.text! += "\nYour bmi : " + text!
+            
+            let bmiDouble = Double(text!)
+            
+            switch bmiDouble!{
+            case ..<18.5 :
+                self.rangeLabel.text = "You're underweight 🥺"
+            case ..<24.9 :
+                self.rangeLabel.text = "You're healthy ☺️"
+            case ..<29.9 :
+                self.rangeLabel.text = "You're overweight 🥺"
+            default:
+                self.rangeLabel.text = "You're obese 🥺"
+            }
+            
+            
+        }
+        
+        
+        present(viewController, animated: true)
+    }
+    
 }
 
