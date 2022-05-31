51366580cf440d90bb8f5b42f74c931a59974274
Aleyna Mandacı <aleynamandaci@gmail.com>
2022-05-31 15:58:20 +0300

    first commit

---
+++ homework-2-aleynamandaci/homework-2-aleynamandaci/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  homework-2-aleynamandaci
+//
+//  Created by Aleyna Mandacı on 31.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var heightField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ homework-2-aleynamandaci/homework-2-aleynamandaci/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  homework-2-aleynamandaci
+//
+//  Created by Aleyna Mandacı on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var explanationText: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
